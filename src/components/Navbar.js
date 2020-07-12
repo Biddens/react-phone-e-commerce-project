f948efcd229e9eec5cd5eb02eca2e057ad36cb65
Biddens <68056550+Biddens@users.noreply.github.com>
2020-07-12 17:45:08 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,10 @@ export default class Navbar extends Component {
   render() {
     return (
       <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
-        {/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */}
         <Link to="/">
-          <img src={logo} alt="store" className="navbar-brand" />
+          <img src="https://onlineteledirectory.com/wp-content/uploads/2019/04/logo9.jpg" alt="store" className="navbar-brand"/>
         </Link>
+
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
