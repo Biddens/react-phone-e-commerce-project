8136d47312e29bca7c6811e4d80dcd851b0d531b
Biddens <68056550+Biddens@users.noreply.github.com>
2020-07-12 17:01:32 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,19 +6,16 @@ import { ButtonContainer } from "./Button";
 export default class Navbar extends Component {
   render() {
     return (
-      <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
-        {/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */}
+      <Nav className="navbar sticky-top navbar-expand-sm  navbar-dark px-sm-5">
         <Link to="/">
-          <img src={logo} alt="store" className="navbar-brand" />
+          <img src="https://onlineteledirectory.com/wp-content/uploads/2019/04/logo9.jpg" alt="store" className="navbar-brand"/>
         </Link>
+
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
-              products
-            </Link>
+             <Link to="/" className="nav-link">
+              <h2 className="company">LWEZA CLAYS LTD</h2>
+             </Link>
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
@@ -35,7 +32,7 @@ https://www.iconfinder.com/Makoto_msk */}
 }
 
 const Nav = styled.nav`
-  background: var(--mainBlue);
+  background: var(--mainBrown);
   .nav-link {
     color: var(--mainWhite) !important;
     font-size:1.3rem;
@@ -46,20 +43,4 @@ const Nav = styled.nav`
       flex-direction: row !important;
 `;
 
-// const ButtonContainer = styled.button`
-//   text-transform: capitalize;
-// font - size: 1.4rem;
-// background: transparent;
-// border: 0.05rem solid var(--lightBlue);
-// border - radius: 0.5rem;
-// padding: 0.2rem 0.5rem;
-// outline - color: red;
-// cursor: pointer;
-// display: inline - block;
-// margin: 0.2rem 0.5rem 0.2rem 0;
-// transition: all 0.5s ease -in -out;
-//   &: hover {
-//   background: var(--lightBlue);
-//   color: var(--mainBlue);
-// }
-// `;
+
